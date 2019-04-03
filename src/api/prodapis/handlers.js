f67830f38ab1b6ca54f83090ecf30208304f0be2
Vishal Nagda <vishalnagda1@gmail.com>
2019-04-04 00:31:16 +0530

    add csv2json function in prodapis handlers file

--- src/api/prodapis/handlers.js
+++ src/api/prodapis/handlers.js
@@ -1,5 +1,7 @@
+const csv = require('csvtojson')
+
 let adder = (sum, element) => {
-	let p = new Promise ((resolve) => {
+  let p = new Promise((resolve) => {
     resolve(sum + element);
   });
 
@@ -8,18 +10,25 @@ let adder = (sum, element) => {
 
 
 export let loop = (request, h) => {
-  let numbers = [1,2,3,4,5,6,7,8,9,10];
+  let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   let sum = 0;
-  
+
   numbers.forEach(n => {
     console.log(`Trying to add ${n}`);
-  	adder(sum, n)
-  		.then(res => {
+    adder(sum, n)
+      .then(res => {
         console.log(`Current sum is ${n}`);
         sum = res
       });
   });
-  
+
   return sum;
+};
+
+export let csv2json = async (request, h) => {
+  // file is the payload name
+  const { file } = request.payload;
+  const jsonArray = await csv().fromString(file);
+  return jsonArray;
 };
 
