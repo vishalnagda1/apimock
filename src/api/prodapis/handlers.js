d127e789eaecade2b7d83a30a7a77a551f877afe
Vishal Nagda <vishalnagda1@gmail.com>
2019-04-07 20:34:30 +0530

    update the loop api code and make it more readable

--- src/api/prodapis/handlers.js
+++ src/api/prodapis/handlers.js
@@ -8,51 +8,17 @@ let adder = (sum, element) => {
   return p;
 }
 
-
-// export let loop = (request, h) => {
-//   let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-//   let sum = 0;
-
-//   numbers.forEach(n => {
-//     console.log(`Trying to add ${n}`);
-//     adder(sum, n)
-//       .then(res => {
-//         console.log(`Current sum is ${n}`);
-//         sum = res
-//       });
-//   });
-
-//   return sum;
-// };
-
-// export let loop = async (request, h) => {
-//   let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-//   let sum = 0;
-//   let i = 0;
-//   while (i < numbers.length) {
-//     console.log(`Trying to add ${numbers[i]}`);
-//     const res = await adder(sum, numbers[i]);
-//     console.log(`Current sum is ${res}`);
-//     sum = res
-//     i += 1;
-//   }
-//   return sum;
-// };
-
 export let loop = async (request, h) => {
   let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   let sum = 0;
-  const getSum = async (i = 0) => {
-    if (i >= numbers.length) {
-      return;
-    }
+  let i = 0;
+  while (i < numbers.length) {
     console.log(`Trying to add ${numbers[i]}`);
     const res = await adder(sum, numbers[i]);
     console.log(`Current sum is ${res}`);
-    sum = res;
-    return getSum(i + 1);
+    sum = res
+    i += 1;
   }
-  await getSum();
   return sum;
 };
 
